a98c40a58abdc3bd1a449faf0df880f80e6ccfd8
fffan64 <ff.fan@hotmail.fr>
2023-01-29 11:27:59 +0900

    fix: og image

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -7,14 +7,16 @@ const defaultMeta = {
   siteName: "Jonathan's portfolio",
   description: 'Portfolio website of Jonathan Vacherat',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  // url: 'https://tsnext-tw.thcl.dev',
+  url: '/',
   type: 'website',
   robots: 'follow, index',
   /**
    * No need to be filled, will be populated with openGraph function
    * If you wish to use a normal image, just specify the path below
    */
-  image: 'https://tsnext-tw.thcl.dev/images/large-og.png',
+  // image: 'https://tsnext-tw.thcl.dev/images/large-og.png',
+  image: '/favicon/android-chrome-384x384.png',
 };
 
 type SeoProps = {
